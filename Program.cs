bb3e768413a55347b7d89802a49c5249c37b3847
= <=>
2024-02-12 11:38:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Random numAleatorio = new Random();
+int numeroSecreto = numAleatorio.Next(1, 101);
+
+Console.WriteLine(">>> - JOGO DO NÚMERO SECRETO - <<<\n");
+
+do {
+    Console.WriteLine("Digite um número entre 1 e 100: ");
+    int randomNum = int.Parse(Console.ReadLine()!);
+
+    if (randomNum == numeroSecreto) {
+        Console.WriteLine("Parabéns! Você acertou!");
+        break;
+    } else if (randomNum < numeroSecreto) {
+        Console.WriteLine("O número é maior! Tente novamente");
+    } else {
+        Console.WriteLine("O número é menor! Tente novamente.");
+    }
+
+} while(true);
+
+Console.WriteLine("Muito bem! O jogo finalizará agora...");
